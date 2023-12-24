cc7cfc2e4dbf39e258e0b87678577868237e7473
chaabat <chaabat.02@gmail.com>
2023-12-24 22:39:12 +0100

    update path

--- app/views/inc/header.php
+++ app/views/inc/header.php
@@ -11,15 +11,14 @@
 <body>
 
 
-
   <aside id="logo-sidebar"
     class="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0"
     aria-label="Sidebar">
     <div class="h-full w-[200px] px-3 py-4 overflow-y-auto bg-[#fdba74] dark:bg-gray-800">
-      <a href="<?php echo URLROOT; ?>" class="flex flex-col items-center ps-3.5 mb-5">
-        <img src="../../../public/photos/logo.png" class="h-12 me-3 sm:h-12" alt="chaabat" />
+      <a href="<#" class="flex flex-col items-center ps-3.5 mb-5">
+        <img src="<?php echo URL_ROOT ?>/photos/logo.png" class="h-12 me-3 sm:h-12" alt="chaabat" />
         <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
-          <img class="w-8 h-8" src="../../public/photos/administrateur.png" alt="">
+          <img class="w-8 h-8" src="<?php echo URL_ROOT ?>/photos/administrateur.png" alt="">
 
 
         </span>
@@ -29,19 +28,19 @@
         <br>
 
         <li>
-          <a href="<?php echo URLROOT; ?>/pages/assurence"
+          <a href="<?php echo URL_ROOT ?>/pages/assurence"
             class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
 
-            <img class="w-8 h-8" src="../../public/photos/batiment.png" alt="">
+            <img class="w-8 h-8" src="<?php echo URL_ROOT ?>/photos/batiment.png" alt="">
 
             <span class="flex-1 ms-3 whitespace-nowrap">Assurences</span>
           </a>
         </li>
 
         <li>
-          <a href="<?php echo URLROOT; ?>/pages/clients"
+          <a href="<?php echo URL_ROOT ?>/pages/clients"
             class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-            <img class="w-8 h-8" src="../../public/photos/ressource.png" alt="">
+            <img class="w-8 h-8" src="<?php echo URL_ROOT ?>/photos/ressource.png" alt="">
 
             <span class="flex-1 ms-3 whitespace-nowrap">Clients</span>
 
@@ -51,7 +50,7 @@
         <li>
           <a href="transactions.php"
             class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-            <img class="w-8 h-8" src="../../public/photos/protection.png" alt="">
+            <img class="w-8 h-8" src="<?php echo URL_ROOT ?>/photos/protection.png" alt="">
 
             <span class="flex-1 ms-3 whitespace-nowrap">Articles</span>
           </a>
@@ -59,7 +58,7 @@
         <li>
           <a href="Agences.php"
             class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-            <img class="w-8 h-8" src="../../public/photos/reclamer.png" alt="">
+            <img class="w-8 h-8" src="<?php echo URL_ROOT ?>/photos/reclamer.png" alt="">
 
 
             <span class="flex-1 ms-3 whitespace-nowrap">Claims</span>
@@ -68,7 +67,7 @@
         <li>
           <a href="banques.php"
             class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-            <img class="w-8 h-8" src="../../public/photos/dirham.png" alt="">
+            <img class="w-8 h-8" src="<?php echo URL_ROOT ?>/photos/dirham.png" alt="">
 
             <span class="flex-1 ms-3 whitespace-nowrap">Primes</span>
           </a>
@@ -80,7 +79,7 @@
         <li>
           <a href="index.php"
             class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-            <img class="w-8 h-8" src="../../public/photos/se-deconnecter.png" alt="">
+            <img class="w-8 h-8" src="<?php echo URL_ROOT ?>/photos/se-deconnecter.png" alt="">
 
 
             <span class="flex-1 ms-3 whitespace-nowrap">Log Out</span>

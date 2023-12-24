55aec0d3c6058aa63abf801fbf6b9f8e0b4938f3
chaabat <chaabat.02@gmail.com>
2023-12-24 15:33:03 +0100

    models/class/clients

---
+++ app/models/clients.php
@@ -0,0 +1,54 @@
+<?php
+
+class clients
+{
+
+    private $id_client;
+    private $nom;
+    private $prenom;
+    private $adress;
+
+    public function __construct($id_client, $nom, $prenom, $adress)
+    {
+        $this->id_client = $id_client;
+        $this->nom = $nom;
+        $this->prenom = $prenom;
+        $this->adress = $adress;
+
+    }
+
+    public function getIdClient()
+    {
+        return $this->id_client;
+    }
+
+    public function getNom()
+    {
+        return $this->nom;
+    }
+    public function setNom($nom)
+    {
+        $this->nome = $nom;
+    }
+
+    public function getPrenom()
+    {
+        return $this->prenom;
+    }
+    public function setPrenom($prenom)
+    {
+        $this->prenom = $prenom;
+    }
+    public function getAdress()
+    {
+        return $this->adress;
+    }
+    public function setAdress($adress)
+    {
+        $this->adress = $adress;
+    }
+
+
+
+}
+?>
